e752a56518029ced1c4c7240dba694010addc6e2
Weidi Zhang <weidiz999@yahoo.com>
2015-08-01 22:44:09 -0700

    Update queue checker for new db structure

--- cronjob/queue_checker.php
+++ cronjob/queue_checker.php
@@ -2,16 +2,15 @@
 // Start: Settings
 $ytdlBin = "C:\\xampp\htdocs\youtube2mp3\cronjob\bin\youtube-dl.exe";
 $ffmpegDir = "C:\\xampp\htdocs\youtube2mp3\cronjob\bin\\";
-
-$outputDir = dirname(__FILE__) . "\..\converted\\"; // include the last slash
 // End: Settings
 
-require dirname(__FILE__) . "/../settings.php";
 set_time_limit(0);
+require dirname(__FILE__) . "/../settings.php";
 
+$outputDir = realpath(dirname(__FILE__) . "/../converted/");
 $db = new PDO("mysql:host=" . $dbHost . ";dbname=" . $dbName . ";charset=utf8", $dbUser, $dbPass);
 
-$queueQuery = $db->prepare("SELECT `ID`,`VideoID` FROM `conversions` WHERE `Queued`=FALSE");
+$queueQuery = $db->prepare("SELECT `ID`,`VideoID` FROM `conversions` WHERE `Started`=FALSE");
 $queueQuery->execute();
 $unqueuedArr = $queueQuery->fetchAll();
 
@@ -19,10 +18,10 @@ foreach ($unqueuedArr as $unqueued) {
 	$reqID = $unqueued["ID"];
 	$reqVideoID = $unqueued["VideoID"];
 	
-	$updateQueue = $db->prepare("UPDATE `conversions` SET `Queued`=TRUE WHERE `ID`=:id");
+	$updateQueue = $db->prepare("UPDATE `conversions` SET `Started`=TRUE, `TimeStarted`=UNIX_TIMESTAMP() WHERE `ID`=:id");
 	$updateQueue->execute(array(":id" => $reqID));
 	
-	$cmd = shell_exec($ytdlBin . " --extract-audio --prefer-ffmpeg --ffmpeg-location " . $ffmpegDir . " --audio-quality 128K --audio-format mp3 -o \"" . $outputDir . "%(id)s.%(ext)s\" -- " . escapeshellarg($reqVideoID));
+	$cmd = shell_exec($ytdlBin . " --extract-audio --prefer-ffmpeg --ffmpeg-location " . $ffmpegDir . " --audio-quality 128K --audio-format mp3 -o \"" . $outputDir . "/%(id)s.%(ext)s\" -- " . escapeshellarg($reqVideoID));
 	if (strpos($cmd, "YouTube said: This video does not exist") !== false) {
 		// Video is non-existent
 		$statusCode = 1;
@@ -40,7 +39,7 @@ foreach ($unqueuedArr as $unqueued) {
 		$statusCode = 4;
 	}
 	
-	$updateStatus = $db->prepare("UPDATE `conversions` SET `StatusCode`=:code, `Completed`=TRUE WHERE `ID`=:id");
+	$updateStatus = $db->prepare("UPDATE `conversions` SET `StatusCode`=:code, `Completed`=TRUE, `TimeCompleted`=UNIX_TIMESTAMP() WHERE `ID`=:id");
 	$updateStatus->execute(array(	
 		":code" => $statusCode,
 		":id" => $reqID
